4645b8dbf28b805309340f807ef17d13c3046dd1
Troyyer <joseph.parel@outlook.com>
2018-04-05 00:45:10 +1000

    more changes

--- cypress/integration/MySuiteCRMTest.js
+++ cypress/integration/MySuiteCRMTest.js
@@ -4,10 +4,81 @@ context('720p resolution', function () {
     beforeEach(function () {
       // run these tests as if in a desktop
       // browser with a 720p monitor
-      cy.viewport(1280, 720)
+        cy.viewport(1280, 720)
+        cy.visit('https://demo.suiteondemand.com/index.php')
+        cy.get('#user_name')
+            .type('WILL')
+        cy.get('#username_password')
+            .type('will')
+        cy.contains('Log In').click() 
+        cy.url()
+            .should('include' , 'module=Home')
+        cy.contains('SUITECRM DASHBOARD').should('be.visible')
+        cy.contains('My Activity Stream')
+            .should('be.visible')
     })
 
-    it('Visits Suite CRM site and fail to login', function() {
+
+    it('login directly thru request', function(){
+        cy.request({
+            method: 'POST',
+            url: 'index.php?module=Users&action=Login',
+            form: true,
+            body:{
+                user_name: 'will',
+                username_password: 'will'
+            }
+        })
+        cy.setCookie('name','ck_login_id_20')
+            .setCookie('value','seed_will_id')
+        cy.visit('?module=Home')
+        cy.url()
+        cy.contains('SUITECRM DASHBOARD').should('be.visible')
+    })
+
+    it.only('Visits Suite CRM site and navigate to Accounts', function() {
+        //arrange
+       
+            cy.contains('All').click()
+            //.trigger('mouseover', 'right')
+            cy.get('.open')
+                    .find('a').contains('Accounts')
+                    .click() 
+               // cy.contains('Accounts').click({ force: true })
+                cy.get('.moduleTitle')
+                    .contains('Accounts')
+                    .should('be.visible')
+                cy.get('.pageNumbers').first()
+                cy.get('table.table-responsive')
+                    .filter('[class*=ListRowS1]').should('have.length', 20)
+                    .each(($el,index,$list) => {
+                            
+
+                    })
+            })
+
+    it('Visits Suite CRM site and login to CRM', function() {
+        //arange
+       
+            cy.contains('All').click()
+            //.trigger('mouseover', 'right')
+            cy.get('.open')
+                    .find('a').contains('Accounts')
+                    .click() 
+               // cy.contains('Accounts').click({ force: true })
+                cy.get('.moduleTitle')
+                    .contains('Accounts')
+                    .should('be.visible')
+                cy.contains('Collaboration').trigger('mouseover').click()
+                cy.get('.open')
+                    .find('a').contains('Email')
+                    .click()    
+                cy.get('.moduleTitle')
+                    .contains('Email')
+                    .should('be.visible')
+                })
+
+        it('Visits Suite CRM site and fail to login', function() {
         //arange
         cy.visit('https://demo.suiteondemand.com/index.php')
         // cy.visit('https://www.australianunity.com.au/')
@@ -19,21 +90,15 @@ context('720p resolution', function () {
         cy.contains('You must specify a valid username and password.')
 
     })
-    it('Visits Suite CRM site and login to CRM', function() {
-        //arange
-        cy.visit('https://demo.suiteondemand.com/index.php')
-        // cy.visit('https://www.australianunity.com.au/')
-        cy.get('#user_name')
-        .type('WILL')
-        cy.get('#username_password')
-        .type('will')
-        cy.contains('Log In').click() 
-        cy.url()
-            .should('include' , 'module=Home')
-            cy.contains('SUITECRM DASHBOARD').should('be.visible')
-            cy.contains('My Activity Stream')
-            .should('be.visible')
 
+    it('is redirected on visit to Login Page when no session', function(){
+
+        cy.visit('?module=Home')
+        cy.url().should('include', 'action=Login')
+        cy.contains('Log In').should('be.visible')
+        cy.get('#user_name').should('have.attr', 'placeholder','Username')
+        cy.get('#username_password').should('have.attr', 'placeholder','Password')
+             
     })
 })
 })
